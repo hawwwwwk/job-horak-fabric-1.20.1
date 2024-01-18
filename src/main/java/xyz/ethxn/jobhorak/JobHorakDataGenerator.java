492dd73af988cf7a6d97edf2d70f63ab05a67f2e
ethan <ethanmahlke@gmail.com>
2024-01-18 13:37:23 -0800

    Initial commit

---
+++ src/main/java/xyz/ethxn/jobhorak/JobHorakDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.ethxn.jobhorak;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JobHorakDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
